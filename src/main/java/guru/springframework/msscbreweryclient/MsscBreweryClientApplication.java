12a60abf9fbce6f58059c2361ddb0bc067f7b9a3
murilo_r <murilo_r@dell.com>
2020-02-19 18:48:05 -0300

    adding configuration properties

--- src/main/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplication.java
+++ src/main/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.msscbreweryclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"guru.springframework.msscbreweryclient"})
+@ConfigurationPropertiesScan("guru.springframework.msscbreweryclient.config")
 public class MsscBreweryClientApplication {
 
     public static void main(String[] args) {
--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbreweryclient.web.config;
 
+import lombok.Value;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -17,15 +19,26 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
+    @Value("${sfg.maxtotalconnections}")
+    private final Integer maxTotalConnections;
+    @Value("${sfg.defaultMaxTotalConnections}")
+    private final Integer defaultMaxTotalConnections;
+    @Value("${sfg.connectionRequestTimeout}")
+    private final Integer connectionRequestTimeout;
+    @Value("${sfg.socketTimeout}")
+    private final Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizer(){ }
+
     public ClientHttpRequestFactory clientHttpRequestFactory(){
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnections);
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients
